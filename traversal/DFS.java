e7f912d8e79142c97ea7a501e14f9ab13500cc52
Vijendra Saini <vijendrasaini0101+github@gmail.com>
2026-07-01 17:14:44 +0530

    dfs call

--- traversal/DFS.java
+++ traversal/DFS.java
@@ -1,5 +1,36 @@
 package traversal;
 
+import java.util.*;
+
 public class DFS {
+    public void isBridge(int V, int[][] edges) {
+        List<List<Integer>> adj = getAdj(V, edges);
+        
+        boolean[] vis = new boolean[V];
+        dfs(0, vis, adj);
+    }
     
+    public void dfs(int node, boolean[] vis, List<List<Integer>> adj) {
+        vis[node] = true;
+        
+        for(int nei : adj.get(node)) 
+            if(!vis[nei])
+                dfs(nei, vis, adj);
+    }
+
+    public List<List<Integer>> getAdj(int V, int[][] edges) {
+        List<List<Integer>> list = new ArrayList<>();
+
+        for (int i = 0; i < V; i++) {
+            list.add(new ArrayList<>());
+        }
+
+        for (int[] e : edges) {
+            int u = e[0], v = e[1];
+            list.get(u).add(v);
+            list.get(v).add(u);
+        }
+
+        return list;
+    }
 }
