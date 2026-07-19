13c53fcbf1f2ace62babcc6b924f07677f5dbbda
Vijendra Saini <vijendrasaini0101+github@gmail.com>
2026-07-19 11:54:15 +0530

    stock dp by lookup table

--- dp/stocks/OneStockManyOrders.java
+++ dp/stocks/OneStockManyOrders.java
@@ -3,6 +3,21 @@ package dp.stocks;
 import java.util.Arrays;
 
 public class OneStockManyOrders {
+    public int maxProfit2(int[] prices) {
+        int n = prices.length;
+
+        int prevBuy = 0, prevSell = 0, currBuy = 0, currSell = 0;
+        for(int i = n - 1; i >= 0; i--) {
+            currSell = Math.max(  prices[i] + prevBuy, prevSell);
+            currBuy = Math.max(- prices[i] + prevSell, prevBuy);
+            
+            prevBuy = currBuy;
+            prevSell = currSell;
+        }
+
+        return prevBuy;
+    }
+
     public int maxProfit(int[] prices) {
         int n = prices.length;
         int k = Arrays.stream(prices).max().orElse(0);
