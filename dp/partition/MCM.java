2dda9faddbac616b3cdd04fadd55c1e0023a5a7c
Vijendra Saini <vijendrasaini0101+github@gmail.com>
2026-07-23 04:57:08 +0530

    mcm tabulation intuition wise

--- dp/partition/MCM.java
+++ dp/partition/MCM.java
@@ -3,6 +3,28 @@ package dp.partition;
 import java.util.Arrays;
 
 public class MCM {
+    static int matrixMultiplication(int arr[]) {
+        int n = arr.length;
+        int[][] dp = new int[n][n];
+        for(int length = 2; length < n; length++) {
+            for(int i = 0; i + length < n; i++) {
+                int j = i + length;
+                
+                int min = Integer.MAX_VALUE;
+                for(int k = i + 1; k < j; k++) {
+                    int leftPart = dp[i][k];
+                    int rightPart = dp[k][j];
+                    int opsAtPart = arr[i] * arr[k] * arr[j];
+                    min = Math.min(min, leftPart + rightPart + opsAtPart);
+                }
+                
+                dp[i][j] = min;
+            }
+        }
+        
+        return dp[0][n - 1];
+    }
+
     static int matrixMultiplicationTabulation(int arr[]) {
         int n = arr.length;
         int[][] dp = new int[n][n];
